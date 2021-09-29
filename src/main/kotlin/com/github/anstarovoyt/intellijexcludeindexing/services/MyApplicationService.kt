ec62cb22d0f05cf613f3e44f1becc41f736bb5f3
GitHub Action <action@github.com>
2021-09-29 10:27:19 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/anstarovoyt/intellijexcludeindexing/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.anstarovoyt.intellijexcludeindexing.services
+
+import com.github.anstarovoyt.intellijexcludeindexing.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
